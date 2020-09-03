c68199cdbc7af02626d5990a054524f5b669a555
bsystem64 <brunobastos_1998@hotmail.com>
2020-09-03 12:15:36 -0300

    add ocorrencia e consultas por cidade

--- src/app/controllers/ocorrencia.js
+++ src/app/controllers/ocorrencia.js
@@ -1,10 +1,34 @@
-function all(req, res){
+const models = require('./../models');
 
+async function all(req, res){
+    res.send(
+        await models.Ocorrencias.findAll()
+    );
 }
 
-function findByCidade(req, res){
+async function findByCidade(req, res){
+    let result;
+    
+    if(req.query && req.query.ano){
+        result =  await models.OcorrenciaCidade.findAll({
+            where: {
+                id_cidade: req.params.cidade,
+                ano: req.query.ano
+            },
+            include: [models.Ocorrencias],
+        })
+    }else{
+        result = await models.OcorrenciaCidade.findAll({
+            where: {
+                id_cidade: req.params.cidade   
+            },
+            include: [models.Ocorrencias],
+        })
+    }
 
+    res.send(result)
 }
+
 
 module.exports = {
     all,
---
+++ src/app/models/ocorrencia.js
@@ -0,0 +1,23 @@
+const {Model} = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+    class Ocorrencia extends Model {
+      static associate() {
+      }
+    }
+
+    Ocorrencia.init({
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true
+        },
+        natureza: DataTypes.STRING
+    },{
+      sequelize,
+      tableName: "tb_ocorrencia",
+      modelName: "Ocorrencias",
+      createdAt: false,
+      updatedAt: false
+    });
+    return Ocorrencia;
+};
