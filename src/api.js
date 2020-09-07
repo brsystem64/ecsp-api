5dba47c50b793e730069aa1402747ab22e529fd6
bsystem64 <brunobastos_1998@hotmail.com>
2020-09-07 12:42:53 -0300

    add cors

--- src/api.js
+++ src/api.js
@@ -2,8 +2,10 @@
 require('dotenv').config();
 const express = require('express');
 const endpoints = require('./endpoints');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 try{
     endpoints(app);
@@ -13,5 +15,4 @@ try{
     process.exit();
 }
 
-
-app.listen(3000);
+app.listen(3001);
